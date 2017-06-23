3c113654aaf604d192803a4eafa5d3629a401f5b
Jake Rayson <web@growdigital.org>
2017-06-23 11:38:04 +0100

    metalsmith-move plugin working now :)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Metalsmith  = require('metalsmith');
 var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
 var permalinks  = require('metalsmith-permalinks');
-var move        = require('metalsmith-move');
+var move        = require('metalsmith-move').default;
 var writemetadata    = require('metalsmith-writemetadata');
 
 Metalsmith(__dirname)
@@ -20,12 +20,12 @@ Metalsmith(__dirname)
   //   pattern: ['*.md', '*.html']
   // }))
   .use(markdown())
-  .use(move({
-    'posts': '{relative}/{base}'
-  }))
   .use(permalinks())
   .use(layouts({
     engine: 'handlebars'
+  }))
+  .use(move({
+    'posts': '{-title}{ext}'
   }))
   .build(function(err, files) {
     if (err) { throw err; }
